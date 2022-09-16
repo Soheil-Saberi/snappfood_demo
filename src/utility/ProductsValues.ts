2c767025792d9fc8498ff18049cfc1d15a1c1d98
soheil-saberi <saberi.soheil74@gmail.com>
2022-09-16 16:46:45 +0430

    Snappfood:ADD:add products values

---
+++ src/utility/ProductsValues.ts
@@ -0,0 +1,150 @@
+import pizzaPic1 from "../assets/images/pizza-1.jpg";
+import pizzaPic2 from "../assets/images/pizza-2.jpg";
+import sandPic1 from "../assets/images/sand-1.jpg";
+import sandPic2 from "../assets/images/sand-2.jpg";
+import dishPic1 from "../assets/images/dish-1.jpg";
+import kababPic1 from "../assets/images/kabab-1.jpg";
+import kababPic2 from "../assets/images/kabab-2.jpg";
+import chikPic1 from "../assets/images/chik-1.jpg";
+import chikPic2 from "../assets/images/chik-2.jpg";
+import pishPic1 from "../assets/images/pish-1.jpg";
+import pishPic2 from "../assets/images/pish-2.jpg";
+import drinkPic1 from "../assets/images/drink-1.jpg";
+import drinkPic2 from "../assets/images/drink-2.jpg";
+import etcPic1 from "../assets/images/etc-1.jpg";
+
+export default [
+  {
+    id: 1,
+    category: 1,
+    title: "پیتزا کراکف",
+    description:
+      "خمیر تازه دست ساز، سس مخصوص، سوسیس کراکف پنیری دودی، قارچ، پنیر ترکیبی، فلفل دلمه، زیتون اسلایس شده",
+    img: pizzaPic1,
+    price: 250000,
+    alt: "pizza-pic1",
+  },
+  {
+    id: 2,
+    category: 1,
+    title: "پیتزا یونانی",
+    description:
+      "خمیر تازه دست ساز، سس مخصوص، گوشت چرخ شده و مزه دار شده، ژامبون گوشت و مرغ 90% دودی، قارچ، پنیر ترکیبی، فلفل دلمه",
+    img: pizzaPic2,
+    price: 200000,
+    alt: "pizza-pic2",
+  },
+  {
+    id: 3,
+    category: 2,
+    title: "ساندویچ هات داگ",
+    description:
+      "هات داگ دودی 90%، پنیر گودا، سس قارچ، مخلفات (کاهو، گوجه، خیارشور)",
+    img: sandPic1,
+    price: 300000,
+    alt: "sand-pic1",
+  },
+  {
+    id: 4,
+    category: 2,
+    title: "ساندویچ چف",
+    description:
+      "فیله طعم دار شده و سوخاری شده با ادویه نرمال، ژامبون 90% مرغ دودی، سس قارچ، پنیر گودا، پنیر موزارلا، مخلفات (کاهو، گوجه، خیارشور)",
+    img: sandPic2,
+    price: 240000,
+    alt: "sand-pic2",
+  },
+  {
+    id: 5,
+    category: 3,
+    title: "بشقاب داغ گوشت",
+    description:
+      "سیب زمینی خلال سرخ شده، گوشت رست شده و مزه دار شده، قارچ، پنیر ترکیبی، سس مخصوص",
+    img: dishPic1,
+    price: 190000,
+    alt: "dish-pic1",
+  },
+  {
+    id: 6,
+    category: 4,
+    title: "ترکی پرسی",
+    description: "گوشت و مرغ مزه دار شده، سس مخصوص، پیاز جعفری، خیارشور",
+    img: kababPic1,
+    price: 210000,
+    alt: "kabab-pic1",
+  },
+  {
+    id: 7,
+    category: 4,
+    title: "ساندویچ ترکی",
+    description: "گوشت و مرغ مزه دار شده، سس مخصوص، پیاز جعفری، خیارشور",
+    img: kababPic2,
+    price: 180000,
+    alt: "kabab-pic2",
+  },
+  {
+    id: 8,
+    category: 5,
+    title: "میگو سوخاری",
+    description:
+      "میگو طعم دار شده و سوخاری شده با ادویه نرمال، سیب زمینی سرخ شده خلال مزه دار شده با ادویه مخصوص، سالاد کلم، نان بروتچن",
+    img: chikPic1,
+    price: 260000,
+    alt: "chik-pic1",
+  },
+  {
+    id: 9,
+    category: 5,
+    title: "بال و پاچین",
+    description:
+      "بال و کتف سوخاری شده و مرینیت شده با ادویه نرمال، سیب زمینی سرخ شده خلال مزه دار شده با ادویه مخصوص، سالاد کلم، نان بروتچن",
+    img: chikPic2,
+    price: 150000,
+    alt: "chik-pic2",
+  },
+  {
+    id: 10,
+    category: 6,
+    title: "سالاد کلم",
+    description: "کلم، هویج، سس مخصوص سالاد کلم",
+    img: pishPic1,
+    price: 50000,
+    alt: "pish-pic1",
+  },
+  {
+    id: 11,
+    category: 6,
+    title: "قارچ سوخاری",
+    description: "قارچ سوخاری شده با روکش مخصوص",
+    img: pishPic2,
+    price: 70000,
+    alt: "pish-pic2",
+  },
+  {
+    id: 12,
+    category: 7,
+    title: "کوکا",
+    description: "",
+    img: drinkPic1,
+    price: 10000,
+    alt: "drink-pic1",
+  },
+  {
+    id: 13,
+    category: 7,
+    title: "فانتا",
+    description: "",
+    img: drinkPic2,
+    price: 10000,
+    alt: "drink-pic2",
+  },
+  {
+    id: 14,
+    category: 8,
+    title: "نان باگت",
+    description: "",
+    price: 5000,
+    img: etcPic1,
+    alt: "etc-pic1",
+  },
+];
