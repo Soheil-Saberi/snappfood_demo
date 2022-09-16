b21ed6f7d68662cf6784583c6b2f04f4072b9db8
soheil-saberi <saberi.soheil74@gmail.com>
2022-09-17 02:13:10 +0430

    Snappfood:FIX:fix screen config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,9 @@ module.exports = {
       },
       screens: {
         mobile: '320px',
-        tablet: '768px',
-        laptop: '1440px',
-        desktop: '1920px'
+        tablet: '1024px',
+        laptop: '1280px',
+        desktop: '1536px'
       }
     }
   },
