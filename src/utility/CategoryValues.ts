e9e3d19707df707d115cd918d210099180ce795f
soheil-saberi <saberi.soheil74@gmail.com>
2022-09-16 13:18:33 +0430

    Snappfood:ADD:add category values

---
+++ src/utility/CategoryValues.ts
@@ -0,0 +1,19 @@
+import pizzaIcon from "../assets/images/pizza.png";
+import sandwichIcon from "../assets/images/sandwich.png";
+import dishIcon from "../assets/images/dish.png";
+import kebabIcon from "../assets/images/kebab.png";
+import chickenIcon from "../assets/images/chicken.png";
+import pishIcon from "../assets/images/pish.png";
+import sodaIcon from "../assets/images/soda.png";
+import etcIcon from "../assets/images/etc.png";
+
+export default [
+  { id: 1, title: "پیتزا", img: pizzaIcon, alt: "pizza-icon" },
+  { id: 2, title: "ساندویچ", img: sandwichIcon, alt: "sandwich-icon" },
+  { id: 3, title: "بشقاب داغ", img: dishIcon, alt: "dish-icon" },
+  { id: 4, title: "کباب ترکی", img: kebabIcon, alt: "kebab-icon" },
+  { id: 5, title: "کنتاکی", img: chickenIcon, alt: "chicken-icon" },
+  { id: 6, title: "پیش غذا", img: pishIcon, alt: "pish-icon" },
+  { id: 7, title: "نوشیدنی", img: sodaIcon, alt: "soda-icon" },
+  { id: 8, title: "سایر", img: etcIcon, alt: "etc-icon" },
+];
