0d92589775f2012f55d58a8aafec3afcb3d88c77
soheil-saberi <saberi.soheil74@gmail.com>
2022-09-16 12:48:16 +0430

    Snappfood:ADD:add right sidebar

--- src/components/layout/Main/Main.tsx
+++ src/components/layout/Main/Main.tsx
@@ -1,11 +1,15 @@
 import { Fragment } from "react";
 //Style
 import styles from "./Main.module.css";
+// Components
+import RightSidebar from "../RightSidebar/RightSidebar";
 
 export default function Main() {
   return (
     <Fragment>
-      <div className={styles.div}></div>
+      <div className={styles.div}>
+        <RightSidebar />
+      </div>
     </Fragment>
   );
 }
---
+++ src/components/layout/RightSidebar/RightSidebar.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+// Style
+import styles from "./RightSidebar.module.css";
+
+export default function RightSidebar() {
+  return (
+    <Fragment>
+      <aside className={styles.aside}></aside>
+    </Fragment>
+  );
+}
