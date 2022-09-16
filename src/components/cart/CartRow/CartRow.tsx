ce5afb79e44e3c0a8546a3703247959df3632792
soheil-saberi <saberi.soheil74@gmail.com>
2022-09-16 17:23:53 +0430

    Snappfood:ADD:complate cart

---
+++ src/components/cart/CartRow/CartRow.tsx
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+// Style
+import styles from "./CartRow.module.css";
+// Utils
+import { camaPrice } from "../../../utility/Utils";
+
+export default function CartRow(props: any) {
+  const { title, price } = props;
+  return (
+    <Fragment>
+      <div className={styles.div}>
+        <p>{title}</p>
+        <p>{camaPrice(price)} تومان</p>
+      </div>
+    </Fragment>
+  );
+}
--- src/components/cart/CartUser/CartUser.tsx
+++ src/components/cart/CartUser/CartUser.tsx
@@ -6,6 +6,12 @@ import styles from "./CartUser.module.css";
 // Utils
 import ProductsValues from "../../../utility/ProductsValues";
 import { camaPrice } from "../../../utility/Utils";
+// Components
+import CartHeader from "../CartHeader/CartHeader";
+import CartRow from "../CartRow/CartRow";
+import CartItem from "../CartItem/CartItem";
+import CartSum from "../CartSum/CartSum";
+import CartButton from "../CartButton/CartButton";
 
 export default function CartUser() {
   const cartUser = useSelector((state: any) => state.cartUser);
@@ -14,11 +20,11 @@ export default function CartUser() {
   const TAX_PRICE = 12000;
   const DELIVERY_PRICE = 15000;
 
-  const priceProduct = (productId: number) => {
+  const priceProduct = (productId: number): number | undefined => {
     return ProductsValues.find((item: any) => item.id === productId)?.price;
   };
 
-  const sumCartUser = () => {
+  const sumCartUser = (): number => {
     return cartUser.reduce((previousValue: any, currentValue: any) => {
       return (
         previousValue +
@@ -32,7 +38,20 @@ export default function CartUser() {
 
   return (
     <Fragment>
-      <div className={styles.div}></div>
+      <div className={styles.div}>
+        <CartHeader cartUser={cartUser} />
+        <div className={styles.divCartItem}>
+          {cartUser.map((item: any) => {
+            return <CartItem key={item.productId} productId={item.productId} />;
+          })}
+        </div>
+        <CartRow title="هزینه دسته بندی" price={PACK_PRICE} />
+        <CartRow title="مالیات" price={TAX_PRICE} />
+        <CartRow title="هزینه ارسال" price={DELIVERY_PRICE} />
+        <CartSum sum={camaPrice(sumCartUser())} />
+        <hr />
+        <CartButton />
+      </div>
     </Fragment>
   );
 }
