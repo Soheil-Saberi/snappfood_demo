9f5399ed3e136de67c20bb2e2932571fccf8a949
soheil-saberi <saberi.soheil74@gmail.com>
2022-09-16 16:59:57 +0430

    Snappfood:ADD:add product item

--- src/components/product/ProductCategory/ProductCategory.tsx
+++ src/components/product/ProductCategory/ProductCategory.tsx
@@ -1,6 +1,10 @@
 import { Fragment } from "react";
 // Style
 import styles from "./ProductCategory.module.css";
+// Utils
+import ProductsValues from "../../../utility/ProductsValues";
+// Components
+import ProductItem from "../ProductItem/ProductItem";
 
 export default function ProductCategory(props: any) {
   const { categoryId, categoryName } = props;
@@ -10,7 +14,21 @@ export default function ProductCategory(props: any) {
         <div className={styles.divTitle}>
           <p>{categoryName}</p>
         </div>
-        <div className={styles.divProducts}></div>
+        <div className={styles.divProducts}>
+          {ProductsValues.filter(
+            (product) => product.category === categoryId
+          ).map((item) => (
+            <ProductItem
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              description={item.description}
+              img={item.img}
+              alt={item.alt}
+              price={item.price}
+            />
+          ))}
+        </div>
       </div>
     </Fragment>
   );
