ac3f7b9c9905b9416200c9a7d33852a81ee75199
soheil-saberi <saberi.soheil74@gmail.com>
2022-09-16 12:52:13 +0430

    Snappfood:ADD:add products

--- src/components/layout/Main/Main.tsx
+++ src/components/layout/Main/Main.tsx
@@ -3,12 +3,14 @@ import { Fragment } from "react";
 import styles from "./Main.module.css";
 // Components
 import RightSidebar from "../RightSidebar/RightSidebar";
+import Products from "../../product/Products/Products";
 
 export default function Main() {
   return (
     <Fragment>
       <div className={styles.div}>
         <RightSidebar />
+        <Products />
       </div>
     </Fragment>
   );
---
+++ src/components/product/Products/Products.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+// Style
+import styles from "./Products.module.css";
+
+export default function Products() {
+  return (
+    <Fragment>
+      <aside className={styles.aside}></aside>
+    </Fragment>
+  );
+}
