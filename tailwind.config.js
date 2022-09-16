5b996e304109abce7fbfc2d0fe4714feb7f2fdfa
soheil-saberi <saberi.soheil74@gmail.com>
2022-09-16 12:12:56 +0430

    Snappfood:ADD:add screen and color to config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,18 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        pink: "#ff00a6",
+        background: "#f9fafb",
+      },
       fontFamily: {
         iransans: ["IranSans"],
+      },
+      screens: {
+        mobile: "320px",
+        tablet: "768px",
+        laptop: "1440px",
+        desktop: "1920px",
       },
     },
   },
