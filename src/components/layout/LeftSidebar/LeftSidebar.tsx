c477916118a1c3a7f33dcf523360aa95949f3162
soheil-saberi <saberi.soheil74@gmail.com>
2022-09-16 17:08:58 +0430

    Snappfood:ADD:add empty cart

--- src/components/layout/LeftSidebar/LeftSidebar.tsx
+++ src/components/layout/LeftSidebar/LeftSidebar.tsx
@@ -3,12 +3,14 @@ import { Fragment } from "react";
 import styles from "./LeftSidebar.module.css";
 // Components
 import DeliveryDetails from "../../delivery/DeliveryDetails/DeliveryDetails";
+import EmptyCart from "../../cart/EmptyCart/EmptyCart";
 
 export default function LeftSidebar() {
   return (
     <Fragment>
       <aside className={styles.aside}>
         <DeliveryDetails deliveryPrice={15000} />
+        <EmptyCart />
       </aside>
     </Fragment>
   );
