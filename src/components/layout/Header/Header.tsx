590687e23b067b2ece9c5dbc26ee04a641e47aea
soheil-saberi <saberi.soheil74@gmail.com>
2022-09-16 12:14:01 +0430

    Snappfood:ADD:add header

---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+//Style
+import styles from "./Header.module.css";
+//Icons
+import logo from "../../../assets/images/snappfood-logo.png";
+import userIcon from "../../../assets/images/user.png";
+import orderIcon from "../../../assets/images/order.png";
+
+export default function Header() {
+  return (
+    <Fragment>
+      <aside className={styles.aside}>
+        <img className={styles.logo} src={logo} alt="snappfood-logo" />
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="جستجو در رستوران"
+        />
+        <div className={styles.divIcon}>
+          <img src={userIcon} alt="user-icon" />
+          <img src={orderIcon} alt="user-icon" />
+        </div>
+      </aside>
+    </Fragment>
+  );
+}
