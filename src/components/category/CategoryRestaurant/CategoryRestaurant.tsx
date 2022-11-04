d07269d74fd75d6ab40869c6f53a9dbd853e4347
soheil-saberi <saberi.soheil74@gmail.com>
2022-11-04 13:18:57 +0330

    Snappfood:FIX:fix category restautant

--- src/components/category/CategoryRestaurant/CategoryRestaurant.tsx
+++ src/components/category/CategoryRestaurant/CategoryRestaurant.tsx
@@ -6,22 +6,13 @@ import CategoryItem from '../CategoryItem/CategoryItem';
 // Utils
 import CategoryValues from '../../../utility/CategoryValues';
 
-import kebabIcon from '../../../assets/images/kebab.png';
-
 export default function CategoryRestaurant() {
-  const categoryValue: any = {
-    id: 1,
-    title: '',
-    img: kebabIcon,
-    alt: 'pizza-icon'
-  };
   return (
     <Fragment>
       <div className={styles.div}>
-        {/* {CategoryValues.map((item) => (
+        {CategoryValues.map((item) => (
           <CategoryItem key={item.id} category={item} />
-        ))} */}
-        <CategoryItem key={categoryValue.id} category={categoryValue} />
+        ))}
       </div>
     </Fragment>
   );
