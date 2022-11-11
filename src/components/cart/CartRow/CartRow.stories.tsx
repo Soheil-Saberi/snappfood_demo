f91db5968bd90b3487f7ff4ac33a132cd49e2dc5
soheil-saberi <saberi.soheil74@gmail.com>
2022-11-11 20:26:27 +0330

    Snappfood:ADD:add cartRow story

---
+++ src/components/cart/CartRow/CartRow.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import CartRow from './CartRow';
+
+export default {
+  component: CartRow,
+  title: 'Cart Row'
+} as ComponentMeta<typeof CartRow>;
+
+export const CartRowStory: ComponentStory<typeof CartRow> = (args) => {
+  return (
+    <div className="mt-4 gap-2 p-4 flex flex-col  border rounded-md shadow-md w-full h-fit">
+      <CartRow title={args.title} price={args.price} />
+    </div>
+  );
+};
+
+CartRowStory.args = {
+  title: 'هزینه دسته بندی',
+  price: '500000'
+};
