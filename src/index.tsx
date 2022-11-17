f5d7d2f7bc8f8c9e0cc8b00409796cfe93bb2e83
soheil-saberi <saberi.soheil74@gmail.com>
2022-11-17 11:34:09 +0330

    Snappfood:ADD:add react-toastify & config

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store/index';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer rtl bodyClassName="toast" />
     </Provider>
   </React.StrictMode>
 );
